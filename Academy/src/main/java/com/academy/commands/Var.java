be84bfa58c2bd612c7a1b990ca82e6a92183dc52
Dihegoof <dihegofn.j@gmail.com>
2023-05-30 17:16:32 -0300

    adicionado comando var

---
+++ Academy/src/main/java/com/academy/commands/Var.java
@@ -0,0 +1,13 @@
+package com.academy.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Var implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return false;
+	}
+}
